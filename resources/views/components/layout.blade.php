2ee92f93b034e110d2ca717ac65e87d90ad1fa8f
Xav-Sathorn <x.coenen.dev@gmail.com>
2022-08-25 15:06:26 +0200

    08-store-listings-data

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -56,7 +56,7 @@
     <p class="ml-2">2022&copy;Xavier Coënen-Corbo</p>
 
     <a
-        href="create.html"
+        href="/listings/create"
         class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
         >Post Job</a
     >
